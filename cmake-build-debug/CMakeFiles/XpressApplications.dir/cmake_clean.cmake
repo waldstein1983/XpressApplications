98330baaee64ae20a3781298860f77be7039a391
waldstein <wangbaohua2009@163.com>
2017-06-16 12:20:04 +0800

    Init commit

---
+++ cmake-build-debug/CMakeFiles/XpressApplications.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/XpressApplications.dir/main.cpp.o"
+  "XpressApplications.pdb"
+  "XpressApplications"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XpressApplications.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
